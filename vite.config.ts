f10f7d562c1c9444b9b82df94858322c926b9cf1
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-09-27 07:23:00 -0400

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/<REPO>/',
+	base: '/<TheRandomGuilds>/',
 	plugins: [react()],
 	resolve: {
 		alias: {
