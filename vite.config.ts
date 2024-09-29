0031cd3f8c94a7866d13b7ab50ed32aee820e600
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2024-09-28 20:09:53 -0400

    removed updated base url configs so it will route to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/<TheRandomGuilds>/',
 	plugins: [react()],
 	resolve: {
 		alias: {
